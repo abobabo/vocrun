5968b56ef4209de059bd8f90887f9ff23ba94c19
Ben Tiden <benjamin@tiden.de>
2020-03-10 17:33:20 +0100

    put vocab into platforms

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import hsk4vocab from '../../assets/vocab/hsk4.json';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
@@ -7,7 +8,16 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 let sceneOptions = {
-  platformStartSpeed: 350,
+  platformStartSpeed: 150,
+  platformTimerDelay: 2000,
+};
+
+const style = {
+  font: '32px Arial',
+  fill: '#ff0044',
+  wordWrap: true,
+  align: 'center',
+  backgroundColor: '#ffff00',
 };
 
 class GameScene extends Phaser.Scene {
@@ -17,37 +27,50 @@ class GameScene extends Phaser.Scene {
 
   private platformGroup: Phaser.GameObjects.Group;
 
+  private vocabCount: number;
+
   constructor() {
     super(sceneConfig);
+    this.vocabCount = 0;
   }
 
-  addPlatform = (displayWidth: number = 200) => {
+  addPlatform = (displayWidth: number = 40) => {
     let platform;
-    platform = this.physics.add.sprite(window.innerWidth / 2, 10, 'platform');
+    platform = this.physics.add.sprite(0, 0, 'platform');
+    const text = this.add.text(
+      0,
+      0,
+      hsk4vocab[this.vocabCount].hanzi as string,
+      style,
+    );
+    text.setOrigin(0.5);
     platform.setImmovable(true);
-    platform.setVelocityY(sceneOptions.platformStartSpeed);
     this.platformGroup.add(platform);
+    platform.displayHeight = displayWidth;
     platform.displayWidth = displayWidth;
+    const container = this.add.container(window.innerWidth / 2, 10, [
+      platform,
+      text,
+    ]);
+    container.setSize(128, 64);
+    this.physics.world.enable(container);
+    const body = container.body as Phaser.Physics.Arcade.Body;
+    body.setVelocityY(sceneOptions.platformStartSpeed);
+    this.vocabCount += 1;
   };
 
   public preload() {
-    this.load.image('platform', 'assets/platform.png');
+    this.load.image('platform', 'assets/img/platform.png');
   }
 
   public create() {
-    const style = {
-      font: '32px Arial',
-      fill: '#ff0044',
-      wordWrap: true,
-      align: 'center',
-      backgroundColor: '#ffff00',
-    };
     this.square = this.add.text(
-      window.innerWidth / 2 - 100,
-      400,
+      window.innerWidth / 2,
+      window.innerHeight - 50,
       'VocabGoesHere',
       style,
     ) as any;
+    this.square.setOrigin(0.5);
     this.physics.add.existing(this.square);
     this.platformGroup = this.add.group({
       removeCallback: function(platform: any) {
@@ -55,7 +78,7 @@ class GameScene extends Phaser.Scene {
       },
     });
     const platformTimer = this.time.addEvent({
-      delay: 1000,
+      delay: sceneOptions.platformTimerDelay,
       callback: this.addPlatform,
       loop: true,
     });
@@ -63,7 +86,6 @@ class GameScene extends Phaser.Scene {
 
   public update() {
     const cursorKeys = this.input.keyboard.createCursorKeys();
-
     if (cursorKeys.right.isDown) {
       this.square.body.setVelocityX(500);
     } else if (cursorKeys.left.isDown) {
