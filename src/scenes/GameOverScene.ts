23b43d96a23c26fdf57fc5e7ac78c76a6b2c1ab0
Ben Tiden <benjamin@tiden.de>
2020-03-18 22:35:20 +0100

    on game over show score and reward

--- src/scenes/GameOverScene.ts
+++ src/scenes/GameOverScene.ts
@@ -15,22 +15,78 @@ export const gameOptions = {
   heartWidth: 48,
 };
 
+const scoreStyle = {
+  font: '48px Arial',
+  fill: 'black',
+  wordWrap: true,
+  align: 'center',
+  backgroundColor: 'white',
+};
+
+const textStyle = {
+  font: '16px Arial',
+  fill: 'black',
+  wordWrap: true,
+  align: 'center',
+  backgroundColor: 'white',
+};
+
 class GameOverScene extends Phaser.Scene {
-  private score;
+  private score: number;
 
   constructor() {
     super(sceneConfig);
   }
+
+  determineReward = (score: number): string => {
+    switch (true) {
+      case score < 20:
+        return 'stones';
+      case score < 40:
+        return 'spoon';
+      default:
+        return 'golden turtle';
+    }
+  };
 
   init(data) {
     this.score = data.score;
     console.log(this.score);
   }
 
-  public preload() {}
+  public preload() {
+    this.load.image('stones', 'assets/img/stones.png');
+    this.load.image('spoon', 'assets/img/spoon.png');
+    this.load.image('golden turtle', 'assets/img/goldenturtle.png');
+  }
 
   public create() {
-    console.log('game over');
+    const score = this.add.text(
+      window.innerWidth / 2,
+      window.innerHeight / 5,
+      `your score: \n ${String(this.score)}`,
+      scoreStyle,
+    );
+    score.setOrigin(0.5);
+    const yourReward = this.add.text(
+      window.innerWidth / 2,
+      window.innerHeight / 3,
+      `your reward:`,
+      textStyle,
+    );
+    yourReward.setOrigin(0.5);
+    const reward = this.determineReward(this.score);
+    const rewardImg = this.add.sprite(
+      window.innerWidth / 2,
+      window.innerHeight / 2,
+      reward,
+    );
+    const rewardText = this.add.text(
+      window.innerWidth / 2,
+      window.innerHeight / 1.5,
+      reward,
+    );
+    rewardText.setOrigin(0.5);
   }
 
   public update() {}
