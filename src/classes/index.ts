70a7d4dc138d26d6f9ce2450d37f69202057a849
Ben Tiden <benjamin@tiden.de>
2020-03-20 22:54:39 +0100

    add barrier and container types

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -2,4 +2,23 @@ export interface VocabRoll {
   [index: number]: number;
   vocabId: number;
   correct?: boolean;
+  type?: ContainerType;
+}
+
+export enum BarrierType {
+  VANILLA,
+  ALL_WRONG,
+  JOKER,
+}
+
+export const BarrierTypes = {
+  vanilla: BarrierType.VANILLA,
+  allwrong: BarrierType.ALL_WRONG,
+  joker: BarrierType.JOKER,
+};
+
+export enum ContainerType {
+  VANILLA,
+  ALL_WRONG,
+  JOKER,
 }
