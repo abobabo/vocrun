543408bce4a1e326a2f1871b084d02876e4c2e77
Ben Tiden <benjamin@tiden.de>
2020-03-26 15:41:10 +0100

    format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-const path = require("path");
-const webpack = require("webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/main.ts",
-    vendors: ["phaser"]
+    app: './src/main.ts',
+    vendors: ['phaser'],
   },
 
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
 
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
 
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
   },
 
   output: {
-    filename: "app.bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
-  mode: "development",
+  mode: 'development',
 
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    https: true
+    contentBase: path.resolve(__dirname, 'dist'),
+    https: true,
   },
 
   plugins: [
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, "index.html"),
-        to: path.resolve(__dirname, "dist")
+        from: path.resolve(__dirname, 'index.html'),
+        to: path.resolve(__dirname, 'dist'),
       },
       {
-        from: path.resolve(__dirname, "assets", "**", "*"),
-        to: path.resolve(__dirname, "dist")
-      }
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'dist'),
+      },
     ]),
     new webpack.DefinePlugin({
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true)
-    })
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
   ],
 
   optimization: {
@@ -58,10 +58,10 @@ module.exports = {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    }
-  }
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
